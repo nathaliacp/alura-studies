d934646aaf815587eb1d173bad65e1c3dd43f1c1
nathaliacp <nathaliapinheiro3@outlook.com>
2024-02-16 22:34:09 -0300

    add logic to show the time of selected task on the clock

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,17 +1,29 @@
-import { timeToSeconds } from "../../common/utils/date";
+import { useEffect, useState } from "react";
+import { timeToSeconds } from "../../common/utils/time";
+import { ITask } from "../../types/task";
 import Button from "../Button";
 import Clock from "./Clock";
 import style from "./Timer.module.scss";
 
-export default function Timer() {
-	console.log("conversao: ", timeToSeconds("01:01:01"));
+interface Props {
+	selected: ITask | undefined
+}
+
+export default function Timer({ selected }: Props) {
+	const [ time, setTime ] = useState<number>();
+
+	useEffect(() => {
+		if (selected?.time) {
+			setTime(timeToSeconds(selected.time));
+		}
+	}, [selected]);
 
 	return(
 		<div className={ style.timer }>
 			<p className={ style.title }>Pick a card and start the timer</p>
 
 			<div className={ style.clockWrapper }>
-				<Clock />
+				<Clock time={time}/>
 			</div>
 
 			<Button>
